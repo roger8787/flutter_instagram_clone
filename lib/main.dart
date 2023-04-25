7621e3ce3bc924e2441a0ab133c9f818b4ae5a3e
Gomez Rogelio (Nokia) <gomez.rogelio@nokia.com>
2023-04-25 17:00:06 -0600

    Adding firebase logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:playground_app_april_2023/screens/login_screen.dart';
 import 'package:playground_app_april_2023/screens/register_screen.dart';
 
-void main() {
+//firebase initialization
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print('Firebase successfully initialized');
   runApp(const MyApp());
 }
 
